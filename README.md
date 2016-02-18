e2ee01d2eac08e16b5e9f53f6790a1de87eb0c7c
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-02-18 14:30:13 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+[![Build Status](https://travis-ci.org/openworm/org.geppetto.model.nwb.png?branch=master)](https://travis-ci.org/openworm/org.geppetto.model.nwb)
+
+<p align="center">
+  <img src="https://dl.dropboxusercontent.com/u/7538688/geppetto%20logo.png?dl=1" alt="Geppetto logo"/>
+</p>
+
+# Geppetto NWB model interpreter
+
+This Geppetto module contains a model interpreter for [NWB](https://crcns.org/NWB/Overview), a format for cellular-based, neurophysiology data.
+Through this module Geppetto has the ability to parse NWB files and display them.
+
+For information about how this fits into [Geppetto](http://www.geppetto.org/) refer to the umbrella project [org.geppetto](https://github.com/openworm/org.geppetto) on GitHub.
---
+++ src/main/java/org/geppetto/model/nwb/NWBModelInterpreterService.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+. * The MIT License (MIT)
+ *
+ * Copyright (c) 2011 - 2015 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+
+package org.geppetto.model.nwb;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.geppetto.core.beans.ModelInterpreterConfig;
+import org.geppetto.core.data.model.IAspectConfiguration;
+import org.geppetto.core.model.AModelInterpreter;
+import org.geppetto.core.model.GeppettoModelAccess;
+import org.geppetto.core.model.ModelInterpreterException;
+import org.geppetto.core.services.registry.ServicesRegistry;
+import org.geppetto.model.GeppettoLibrary;
+import org.geppetto.model.ModelFormat;
+import org.geppetto.model.types.Type;
+import org.geppetto.model.values.Pointer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author matteocantarelli
+ * 
+ */
+@Service
+public class NWBModelInterpreterService extends AModelInterpreter
+{
+
+	@Autowired
+	private ModelInterpreterConfig nwbModelInterpreterConfig;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geppetto.core.model.IModelInterpreter#getName()
+	 */
+	@Override
+	public String getName()
+	{
+		return this.nwbModelInterpreterConfig.getModelInterpreterName();
+	}
+
+	@Override
+	public void registerGeppettoService()
+	{
+		List<ModelFormat> modelFormats = new ArrayList<ModelFormat>(Arrays.asList(ServicesRegistry.registerModelFormat("NWB")));
+		ServicesRegistry.registerModelInterpreterService(this, modelFormats);
+	}
+
+	@Override
+	public List<ModelFormat> getSupportedOutputs(Pointer pointer) throws ModelInterpreterException
+	{
+		List<ModelFormat> supportedOutputs = super.getSupportedOutputs(pointer);
+		supportedOutputs.add(ServicesRegistry.getModelFormat("NWB"));
+		return supportedOutputs;
+	}
+
+	@Override
+	public Type importType(URL url, String typeName, GeppettoLibrary library, GeppettoModelAccess commonLibraryAccess) throws ModelInterpreterException
+	{
+
+		dependentModels.clear();
+
+		Type nwcModeType = null;
+
+		// Nitesh: convert from NWB to Type
+
+		return nwcModeType;
+
+	}
+
+	@Override
+	public File downloadModel(Pointer pointer, ModelFormat format, IAspectConfiguration aspectConfiguration) throws ModelInterpreterException
+	{
+		throw new ModelInterpreterException("Download model not implemented for NWB model interpreter");
+	}
+
+}
